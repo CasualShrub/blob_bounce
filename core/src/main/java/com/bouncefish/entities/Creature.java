79446670a4e57cf4f7d24c3018eb619772380038
CasualShrub <120472172+CasualShrub@users.noreply.github.com>
2026-03-18 14:45:43 +0800

    Add base creature parent class

---
+++ core/src/main/java/com/bouncefish/entities/Creature.java
@@ -0,0 +1,11 @@
+package com.bouncefish.entities;
+
+public class Creature {
+    int xPosition;
+    int yPosition;
+    float width;
+    float height;
+    float xVelocity;
+    float yVelocity;
+    int pointsGiven;
+}

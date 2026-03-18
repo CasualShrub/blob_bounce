afcf9786558599373d9ec881beb9c92c3649f18c
CasualShrub <120472172+CasualShrub@users.noreply.github.com>
2026-03-18 14:19:12 +0800

    Add placeholder blob asset

--- core/src/main/java/com/bouncefish/Main.java
+++ core/src/main/java/com/bouncefish/Main.java
@@ -1,26 +1,33 @@
 package com.bouncefish;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
 
+    private int screenWidth;
+
+
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        image = new Texture("blob1.png");
+
+        OrthographicCamera camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 400);
+
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 140, 210);
+        batch.draw(image, 540, 210);
         batch.end();
     }
 

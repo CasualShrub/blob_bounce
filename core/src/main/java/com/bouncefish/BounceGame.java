ae446a3f2773db9df9d9150f5c77da942233853d
CasualShrub <120472172+CasualShrub@users.noreply.github.com>
2026-03-29 21:20:55 +0800

    Added debug toggle button

--- core/src/main/java/com/bouncefish/BounceGame.java
+++ core/src/main/java/com/bouncefish/BounceGame.java
@@ -19,8 +19,6 @@ public class BounceGame implements GestureDetector.GestureListener {
     private ArrayList<Creature> _creatureList;
 
     public BounceGame() {
-        Gdx.input.setInputProcessor(new GestureDetector(this));
-
         _bounceFish = new BounceFish();
         _creatureList = new ArrayList<Creature>();
         //make this a wave system???

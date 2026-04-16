32ceb1b7f75d6634d27f604887782a44a2e5fd8d
CasualShrub <120472172+CasualShrub@users.noreply.github.com>
2026-04-16 14:58:23 +0800

    Spawner code documentation

--- core/src/main/java/com/bouncefish/gameplay/CreatureSpawner.java
+++ core/src/main/java/com/bouncefish/gameplay/CreatureSpawner.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
-// This class is responsible for handling the spawn timing and types of creatures based on game time / points
+// This class is responsible for handling queueing waves and creatures up to be spawned into the game
 public class CreatureSpawner {
 
     // This is the queue of creatures that will be spawned but haven't spawned yet
@@ -20,7 +20,6 @@ public class CreatureSpawner {
     private float spawnTimer;
     private Wave currentWave;
     private int currentWaveIndex;
-    private float waveTimer; // Timer that resets to 0 when a new wave is spawned.
     private boolean isActive = false;
 
     public CreatureSpawner(ArrayList<Creature> creatureList) {
@@ -31,7 +30,6 @@ public class CreatureSpawner {
         this.spawnQueue = new PriorityQueue<Creature>(Comparator.comparingDouble(Creature::getSpawnTime));
         this.spawnTimer = 0;
         this.currentWaveIndex = 0;
-        this.waveTimer = 0;
 
         //Populate creatureList with the first wave
         loadWave(this.currentWaveIndex);
@@ -41,14 +39,15 @@ public class CreatureSpawner {
         this.isActive = isActive;
     }
 
+    // Every render frame, query the queue to see if a creature needs to be spawned
     public void handleTimeStep(){
         if (!this.isActive){
             return;
         }
 
+        // If the queue is empty and the wave is over, load the next wave
         if (this.spawnQueue.isEmpty() && spawnTimer >= currentWave.getWaveDuration()){
             if (this.currentWaveIndex < GameConstants.MAXIMUM_HARDCODED_WAVES - 1){
-                System.out.println("Alex: Spawning wave!");
                 this.currentWaveIndex++;
                 this.spawnTimer = 0;
                 loadWave(this.currentWaveIndex);
@@ -58,11 +57,13 @@ public class CreatureSpawner {
 
         this.spawnTimer += Gdx.graphics.getDeltaTime();
 
+        // Spawn any creatures whose spawn times have passed
         if (!this.spawnQueue.isEmpty() && this.spawnTimer >= this.spawnQueue.peek().getSpawnTime()){
             this.creatureList.add(this.spawnQueue.poll());
         }
     }
 
+    // Clear any creatures that have left the screen
     public void cleanUpCreatures() {
         for (int i = this.creatureList.size() - 1; i > 0; i--) {
             Creature creature = this.creatureList.get(i);
@@ -72,16 +73,11 @@ public class CreatureSpawner {
         }
     }
 
+    // Instantiate all the creatures in the given wave and add them to the spawning queue
     private void loadWave(int waveIndex){
         this.currentWave = WaveRegistry.getWave(waveIndex);
         for (CreatureSpawnData<?> spawnData : this.currentWave.getCreaturesToSpawn()) {
             this.spawnQueue.add(spawnData.createInstance());
         }
     }
-
-    public ArrayList<Creature> spawnWave(int waveId) {
-        return null;
-    }
-
-
 }

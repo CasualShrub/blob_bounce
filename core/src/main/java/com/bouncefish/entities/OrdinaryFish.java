b3ad66af8a62210b506cd01ba8ea3c62e8278805
limer-10 <756778115@qq.com>
2026-04-29 10:10:19 +1000

    splash position fix
    fixed the position of the splash when ordinary fish falls into water

--- core/src/main/java/com/bouncefish/entities/OrdinaryFish.java
+++ core/src/main/java/com/bouncefish/entities/OrdinaryFish.java
@@ -81,7 +81,8 @@ public abstract class OrdinaryFish extends Creature{
             Water.playSplash(creature.xPosition);
         }else if(dy < 0 && !creature.inWater && creature.yPosition<=GameConstants.WATER_LEVEL){
             creature.inWater = true;
-            Water.playSplash(creature.xPosition + 100); //used a magic number; using this number makes the visual effect better
+            int xOffset = creature.movingLeft? -100:100; //magic number to fix the position of the splash
+            Water.playSplash(creature.xPosition+xOffset);
         }
 
     }
